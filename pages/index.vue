0b738a4b239b6ff80038dbbf25451f79fb9ea2a7
Enrique De Ridder <riamap23@gmail.com>
2021-09-11 11:11:50 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script lang="js">
+export default ({
+  name: "index",
+  beforeMount() {
+    return this.$router.push('/users');
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/users/_id/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="mt-8">
+    <UserContent :user="userData"></UserContent>
+
+    <v-row class="">
+      <v-col cols="12" class="d-flex justify-end">
+        <ModalCreateUser method="update">
+          <v-btn plain>Edit</v-btn>
+        </ModalCreateUser>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script lang="js">
+import Vue from "vue";
+import {ApiUsers, IUser} from "~/repositories/Users";
+import ModalCreateUser from "~/components/modals/ModalCreateUser.vue";
+
+export default {
+  name: "id",
+  components: {
+    ModalCreateUser
+  },
+  computed: {
+    userData() {
+      return this.$store.state.user.user
+    }
+  },
+  async asyncData({params}) {
+    const idUser = params.id;
+    const user = await ApiUsers.getOneUser(idUser);
+
+    return {user}
+  },
+  beforeMount() {
+    this.$store.commit('user/setUser', this.user);
+  }
+}
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 12px;
+}
+
+p {
+  margin-bottom: 8px;
+}
+</style>
